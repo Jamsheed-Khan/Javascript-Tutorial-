2f949bf58647488879c1cf6ccd63de966e1120f1
Jamsheed-Khan <jamshedkh365@gmail.com>
2024-08-05 02:11:17 +0500

    If statements .....

---
+++ If_statement/app.js
@@ -0,0 +1,19 @@
+// 1.mathimatical operators (+,-,*,%)
+// 2.contional operators (>,<,==,===,!=,<=)
+// 3.logical operators (&&,||)
+
+if(2==2){
+    document.write("2 is equal to 2","<br>")
+}
+
+
+if(3>2){
+    document.write("3 is greater to 2","<br>")
+}
+
+if(2<3){
+    document.write("2 is smaller to 3","<br>")
+}
+if(2!=3){
+    document.write("2 is not equal  to 3","<br>")
+}
---
+++ variables/app.js
@@ -0,0 +1,8 @@
+var abc = +prompt("enter your first number")
+var bca = +prompt("enter your second number")
+document.write(abc+bca)
+// alert(a
+
+
+// user say 3 input lany han first number ,second operator (-,+,*,/,%) third number 
+// document.write(a+b+c)
